ef0372bc18e72228cbef0f2993e191259368b69f
Sushil <skshinde2028@gmail.com>
2023-04-09 16:59:20 +0530

    [Sushil] Comparison between two lines

--- Line Comparison/Program.cs
+++ Line Comparison/Program.cs
@@ -12,19 +12,33 @@ namespace Line_Comparison
         {
             Console.WriteLine("Welcome to the Line comparison computation program");
             Console.ReadLine();
-            //UC1
-            //taking co ordinates from user to find length
-            Console.WriteLine("Enter the x1 co_ordibate");
-            float x1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the y1 co_ordibate");
-            float y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the x2 co_ordibate");
+            //UC2
+            int x0 = 5, y0 = 6, x1 = 8, y1 = 4;
+            Console.WriteLine("Co-ordinates of first line are x0:{0}, y0:{1}, x1:{2}, y2:{3}",x0,y0,x1,y1);
+            float lenght1 = Convert.ToInt32(Math.Sqrt(Math.Pow(x1 - x0, 2) + Math.Pow(y1 - y0, 2)));
+            Console.WriteLine("The legth of first line is: " + Math.Round(lenght1 * 10000.0) / 10000.0);
+            //taking co-ordinates of second line from user to find length
+            Console.WriteLine("Enter the co-ordinates of second line");
+            Console.WriteLine("Enter the x1 co-ordibate");
             float x2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the y2 co_ordibate");
+            Console.WriteLine("Enter the y1 co-ordibate");
             float y2 = Convert.ToInt32(Console.ReadLine());
-            float lenght = Convert.ToInt32(Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2)));
-            Console.WriteLine(Math.Round(lenght*10000.0)/10000.0);
-            Console.ReadLine() ;    
+            Console.WriteLine("Enter the x2 co-ordibate");
+            float x3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the y2 co-ordibate");
+            float y3 = Convert.ToInt32(Console.ReadLine());
+            float lenght2 = Convert.ToInt32(Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2)));
+            Console.WriteLine("The legth of second line is: " + Math.Round(lenght2 * 10000.0) / 10000.0);
+            Console.ReadLine();
+            if (lenght1 == lenght2) 
+            {
+                Console.WriteLine("Two lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Two lines are not equal");
+            }
+            Console.ReadLine();
         }
     }
 }
