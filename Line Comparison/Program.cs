bde25e1ed373c459883a14b8ed6ec579fe3b8894
Sushil <skshinde2028@gmail.com>
2023-04-09 13:32:06 +0530

    [Sushil] uploading main branch

---
+++ Line Comparison/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line comparison computation program");
+            Console.ReadLine();
+        }
+    }
+}
