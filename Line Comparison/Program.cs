172f053714b5078d32fe5ada6e17f939cb81fdae
Sushil <skshinde2028@gmail.com>
2023-04-23 14:43:21 +0530

    [Sushil] Using builtin method to compair two lines

--- Line Comparison/Program.cs
+++ Line Comparison/Program.cs
@@ -12,11 +12,20 @@ namespace Line_Comparison
         {
             Console.WriteLine("Welcome to the Line comparison computation program");
             Console.ReadLine();
-            //UC3
-            int x0 = 5, y0 = 7, x1 = 8, y1 = 4;
-            Console.WriteLine("Co-ordinates of first line are x0:{0}, y0:{1}, x1:{2}, y2:{3}",x0,y0,x1,y1);
-            float lenght1 = Convert.ToInt32(Math.Sqrt(Math.Pow(x1 - x0, 2) + Math.Pow(y1 - y0, 2)));
-            Console.WriteLine("The legth of first line is: " + Math.Round(lenght1 * 10000.0) / 10000.0);
+            //UC4  
+            //taking co-ordinates of first line from user to find length
+            Console.WriteLine("Enter the co-ordinates of first line");
+            Console.WriteLine("Enter the x0 co-ordinate");
+            float x0 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the y0 co-ordinate");
+            float y0 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the x1 co-ordinate");
+            float x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the y1 co-ordinate");
+            float y1 = Convert.ToInt32(Console.ReadLine());
+            float length1 = Convert.ToInt32(Math.Sqrt(Math.Pow(x1 - x0, 2) + Math.Pow(y1 - y0, 2)));
+            Console.WriteLine("The legth of first line is: " + length1);
+
             //taking co-ordinates of second line from user to find length
             Console.WriteLine("Enter the co-ordinates of second line");
             Console.WriteLine("Enter the x1 co-ordinate");
@@ -28,13 +37,14 @@ namespace Line_Comparison
             Console.WriteLine("Enter the y2 co-ordinate");
             float y3 = Convert.ToInt32(Console.ReadLine());
             float lenght2 = Convert.ToInt32(Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2)));
-            Console.WriteLine("The legth of second line is: " + Math.Round(lenght2 * 10000.0) / 10000.0);
+            Console.WriteLine("The legth of second line is: " + lenght2);
             Console.ReadLine();
-            if (lenght1 >= lenght2) 
+            int result = length1.CompareTo(lenght2);
+            if (result > 0)
             {
                 Console.WriteLine("The leght of first line is greater then second line.");
             }
-            else if(lenght1 <= lenght2)
+            else if (result < 0)
             {
                 Console.WriteLine("The length of first line is less than second line.");
             }
